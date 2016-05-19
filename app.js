a9dd21cc97bda0d1120aced97add5595a2e1dbee
Jordy IJsselstijn <j.ijsselstijn@codecast.nl>
2016-05-19 11:24:22 +0200

    Added pirate.js file.
    - listen : listens for orientation changes and emits an event for front-end applications as well.
    - on : on event
    - off : remove event
    - emit : emit event

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 var express = require('express');
-
 var app = require('express')();
-
 var http = require('http').Server(app);
-
 var io = require('socket.io')(http);
 
     app.use(express.static(__dirname + '/public'));
     app.use('/bower_components',  express.static(__dirname + '/bower_components'));
+    app.use('/',  express.static(__dirname + '/'));
 
     app.get('/', function(req, res){
       res.sendFile(__dirname + '/index.html');
---
+++ pirate.js
@@ -0,0 +1,58 @@
+var pirate = (function(window){
+
+    var socket = io();
+
+    return {
+        registeredEvents : {},
+        listen : function(){
+            window.addEventListener('deviceorientation', function(event){
+
+                var max = 80;
+                var min = -80;
+                var ammount = '';
+
+                if(event.beta >= 0){
+                    ammount = (event.beta / max) * 100;
+                    socket.emit('backward', Math.round(ammount) );
+                    this.emit('backward', Math.round(ammount) );
+                }else{
+                    ammount = (event.beta / min) * 100;
+                    socket.emit('forward', Math.round(ammount) );
+                    this.emit('forward', Math.round(ammount) );
+                }
+                if(event.gamma >= 0){
+                    ammount = (event.gamma / max) * 100;
+                    socket.emit('right', Math.round(ammount) );
+                    this.emit('right', Math.round(ammount) );
+                }else{
+                    ammount = (event.gamma / min) * 100;
+                    socket.emit('left', Math.round(ammount) );
+                    this.emit('left', Math.round(ammount) );
+                }
+            });
+        },
+        on : function(eventName, fn){
+            this.registeredEvents[eventName] = this.registeredEvents[eventName] || [];
+            this.registeredEvents[eventName].push(fn);
+        },
+        off: function(eventName, fn){
+            if (this.registeredEvents[eventName]) {
+                for (var i = 0; i < this.registeredEvents[eventName].length; i++) {
+                    if (this.registeredEvents[eventName][i] === fn) {
+                        this.registeredEvents[eventName].splice(i, 1);
+                        break;
+                    }
+                }
+            }
+        },
+        emit : function(eventName, data){
+            if (this.registeredEvents[eventName]) {
+                this.registeredEvents[eventName].forEach(function(fn) {
+                    fn(data);
+                });
+            }
+        }
+
+    }
+
+})(window);
