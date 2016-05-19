4badeb96e4559c0594674582ee33648668898508
Jordy IJsselstijn <j.ijsselstijn@codecast.nl>
2016-05-19 12:30:42 +0200

    Added angular, angular-ui and ui-router to the project. And created the full app structure.

---
+++ public/js/app/app.js
@@ -0,0 +1,15 @@
+var app = angular.module('app', ['ui','ui.router']);
+    app.config(function($stateProvider){
+
+        $stateProvider.state('intro', {
+            url : '/intro',
+            templateUrl : 'public/js/app/templates/intro.html'
+        });
+
+
+    });
+    app.controller('AppController', ['$scope', function($scope){
+
+
+
+    }]);
